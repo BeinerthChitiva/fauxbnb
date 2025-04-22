2a543ed3ba647da342bea39cabc8cf8229eef0fe
BeinerthChitiva <beinerthps4@gmail.com>
2025-04-21 22:47:59 -0500

    Update: ProjectSetup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-indigo-600 text-white flex items-center justify-center text-4xl">
+      Tailwind is ALIVE ⚡️
     </div>
   );
 }
